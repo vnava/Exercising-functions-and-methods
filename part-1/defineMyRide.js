362d605f66ffcf9d3acb2077fa59b0644536e0a9
Vanessa Nava <vnava26@gmail.com>
2016-09-06 21:03:35 -0700

    added define my ride

--- part-1/defineMyRide.js
+++ part-1/defineMyRide.js
@@ -14,14 +14,35 @@ var myRide = {
 
 	//YOUR CODE HERE
 	
+howFast: function(){
+	console.log(" I am on gear ", myRide.gear, " and I can go ", myRide.maxSpeed());
+}, 
 
+shiftUp : function(){
+	myRide.gear++;
+	console.log("Gear now at ", myRide.gear);
+}, 
 
+shiftDown : function(){
+	myRide.gear--;
+	console.log("Gear now at ", myRide.gear);
+}, 
 
-
-
-
+carRemote : function(){
+	if (myRide.locked === true){
+		console.log("Car is unlocked");
+		myRide.locked = false;
+	}
+	else if (myRidelocked === false){
+		console.log("Car is locked");
+		myRide.locked = true;
+	}
+},
 
 
 	//YOUR CODE ENDS HERE
 
 }
+myRide.howFast();
+myRide.shiftUp();
+myRide.carRemote();
